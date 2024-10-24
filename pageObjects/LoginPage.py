e9fad3bb7eca635a56c4a2881bc7f6d3ff283106
ChenniDJack <chenniveera.s@gmail.com>
2024-10-24 17:06:41 +0530

    added testcases

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -1,3 +1,27 @@
-class Login:
+class LoginPage:
 
-    username_textbox = ""
+    username_textbox_id = "Email"
+    password_textbox_id = "Password"
+    submit_button_xpath = "//button[text()='Log in']"
+
+    Logout_button_xpath = "//a[text()='Logout']"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def setUserName(self, username):
+        self.driver.find_element_by_id(self.username_textbox_id).clear()
+        self.driver.find_element_by_id(self.username_textbox_id).send_keys(username)
+
+    def setPassword(self, password):
+        self.driver.find_element_by_id(self.password_textbox_id).clear()
+        self.driver.find_element_by_id(self.password_textbox_id).send_keys(password)
+
+    def clickLogin(self):
+        self.driver.find_element_by_xpath(self.submit_button_xpath).click()
+
+    def clickLogout(self):
+        self.driver.find_element_by_id(self.Logout_button_xpath).click()
+
+
+
